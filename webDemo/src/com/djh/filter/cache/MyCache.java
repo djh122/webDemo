a755d8df8eff4bc05556e04e9b1a8de400a82f62
admin <admin@asus>
2015-09-18 15:14:05 +0800

    dd

--- webDemo/src/com/djh/filter/cache/MyCache.java
+++
@@ -1,5 +0,0 @@
-package com.djh.filter.cache;
-
-public class MyCache {
-	
-}
--- webDemo/src/com/djh/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.djh.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface UserService {
-
-	public boolean valid();
-}
